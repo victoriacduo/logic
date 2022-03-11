1bde269ff2166bd410a379d0876c0acc867d0d9c
victoriacduo <victoriacduo@outlook.com>
2022-03-11 11:06:19 -0300

    att

---
+++ FPOO/aula16/src/modelo/Aluno.java
@@ -0,0 +1,36 @@
+package modelo;
+
+import java.sql.Date;
+
+public class Aluno {
+	
+	public static void main(String[] args) {
+		
+		int ra;
+		String name;
+		Date nascimento;
+		int notas[];
+		notas = new int[4]; 
+		
+		calcIdade();
+		obterConceito();
+		paraString();
+		
+	}
+
+	private static void calcIdade() {
+		
+		
+	}
+
+	private static void obterConceito() {
+		
+		
+	}
+
+	private static void paraString() {
+		
+		
+	}
+
+}
---
+++ FPOO/aula16/src/modelo/Nota.java
@@ -0,0 +1,24 @@
+package modelo;
+
+public class Nota {
+
+	public static void main(String[] args) {
+		float notas[];
+		notas =  new int[3];
+		
+		obterMedia();
+		paraString();
+
+	}
+
+	private static void obterMedia() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void paraString() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
